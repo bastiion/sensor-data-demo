8183be89d49f50153ca4d7268bae9821fcefdfb2
Winzlieb <winzlieb@c3d2.de>
2024-11-05 12:20:06 +0100

    feat: Add search state management with Zustand

--- apps/semantic-desktop-search/src/components/ui/Search.tsx
+++ apps/semantic-desktop-search/src/components/ui/Search.tsx
@@ -30,14 +30,3 @@ export const Search = ({
     </InputGroup>
   )
 }
-import { create } from 'zustand'
-
-interface SearchState {
-  searchQuery: string
-  setSearchQuery: (query: string) => void
-}
-
-export const useSearchStore = create<SearchState>((set) => ({
-  searchQuery: '',
-  setSearchQuery: (query) => set({ searchQuery: query }),
-}))
